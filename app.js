2e5c428af0af6c1d091165ce1b9445a08bf39b40
Deltron18 <karans0518@gmail.com>
2025-08-31 23:43:39 +0530

    create user

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
-// const express = require('express');
-// const app = express();
 
-// const userModel = require('./usermodel')
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.get('/create', async(req, res) => {
-// let createduser =  await userModel.create({
-//     name: 'karan',
-//     email: 'john@example.com',
-//     age: 30
-//   })
-//   res.send(createduser);
-// });
-
-// app.get('/read', async(req, res) => {
-//   let readuser = await userModel.find();
-//   res.send(readuser);
-// });
-
-// app.get('/update', async(req, res) => {
-//  let updateduser= await userModel.findOneAndUpdate({name:"karan"},{name:"karan singh"},{new:true})
-//  res.send(updateduser);
-// });
-
-// app.get('/delete', async(req, res) => {
-//   let deleteduser = await userModel.findOneAndDelete({name:"karan"});
-//   res.send(deleteduser);
-// });
-
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -49,13 +13,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/read', (req, res) => {
-  res.render('read');
+app.get('/read', async (req, res) => {
+  let alluser = await userModel.find();
+  res.render('read', { users: alluser });
 });
 
-app.post('/create', (req, res) => {
-  let{name , email,imageURL} = req.body;
-  
+app.get('/delete/:id', async (req, res) => { 
+  let alluser = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect('/read');
+});
+
+app.post('/create', async(req, res) => {
+  let{name , image,email} = req.body;
+  let createduser = await userModel.create({name,image,email});
+  res.redirect('/read');
 });
 
 
